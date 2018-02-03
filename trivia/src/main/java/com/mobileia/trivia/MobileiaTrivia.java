2d9442e21f8a237311a730344ebf6989a45a73e6
Matias Camiletti <matias.camiletti@gmail.com>
2018-02-03 09:57:43 -0300

    Creado primer archivo para la entidad.

---
+++ trivia/src/main/java/com/mobileia/trivia/MobileiaTrivia.java
@@ -0,0 +1,8 @@
+package com.mobileia.trivia;
+
+/**
+ * Created by matiascamiletti on 3/2/18.
+ */
+
+public class MobileiaTrivia {
+}
---
+++ trivia/src/main/java/com/mobileia/trivia/entity/Trivia.java
@@ -0,0 +1,22 @@
+package com.mobileia.trivia.entity;
+
+import java.util.Date;
+
+/**
+ * Created by matiascamiletti on 3/2/18.
+ */
+
+public class Trivia {
+
+    public int id;
+
+    public String title;
+
+    public String caption;
+
+    public String photo;
+
+    public Date start_date;
+
+    public Date end_date;
+}
